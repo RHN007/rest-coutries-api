e10582353078121d6eb95bed4a3cb38491c9131e
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-03-15 09:11:53 +0600

    adding few lines by destructuring

--- js/countries.js
+++ js/countries.js
@@ -12,14 +12,38 @@ const displayCountries = (countries) => {
     container.innerHTML = countiresHTML.join(" ");
 };
 
-const getCountryHtml = (country) => {
-    return ` <div class ='country'>
-            <h2>${country.name.common}</h2>
+//Orginal
+// const getCountryHtml = (country) => {
+//     return ` <div class ='country'>
+//             <h2>${country.name.common}</h2>
 
-            <img src=${country.flags.png} >
-    
-    
-    
+//             <img src=${country.flags.png} >
+
+//     </div> `;
+// };
+
+// //Option 1
+
+// const getCountryHtml = (country) => {
+//     //Option 1
+//     const { name, flags } = country;
+
+//     return ` <div class ='country'>
+//             <h2>${name.common}</h2>
+
+//             <img src=${flags.png} >
+
+//     </div> `;
+// };
+
+//Option 2 Destructuing in Parameters
+
+const getCountryHtml = ({ name, flags, area, region }) => {
+    return ` <div class ='country'>
+            <h2>${name.common}</h2>
+            <p>Area: ${area}</p>
+            <p> Region: ${region} </p>
+            <img src=${flags.png} >
     </div> `;
 };
 
